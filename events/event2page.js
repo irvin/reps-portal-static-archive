d3860faf7296356f3ef336362f1f7dd781b9d79a
Irvin <irvinfly@gmail.com>
2020-06-03 02:54:05 +0800

    event2page: sort and support year as cmd argv

--- events/event2page.js
+++ events/event2page.js
@@ -7,7 +7,7 @@ template = handlebars.compile(template);
 let jsonData = require('./EVENTS.json');
 console.log('loaded', jsonData.length)
 
-let year = '2015';
+let year = process.argv[2] || '2020';
 let fn = `events_${year}.html`;
 
 var yearEvents =jsonData.filter((event) => {
@@ -27,13 +27,14 @@ fs.writeFileSync(fn, buffer);
 
 let getMonthStr = (dateObj) => ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'][dateObj.getMonth()];
 
+yearEvents.forEach(evt => { evt.start = new Date(evt.start); });
+yearEvents = yearEvents.sort((evt1, evt2) => evt1.start - evt2.start);
+
 yearEvents.forEach(evt => {
 
-    var dtStart = new Date(evt.start);
+    var dtStart = evt.start;
     var dtEnd = new Date(evt.end);
 
-    var startDayStr = `${dtStart.getFullYear()} ${dtStart.getMonth()+1} ${dtStart.getDate()}`;
-    var endDayStr = `${dtEnd.getFullYear()} ${dtEnd.getMonth()+1} ${dtEnd.getDate()}`;
     evt.start_day = dtStart.getDate();
     evt.end_day = dtEnd.getDate();
     evt.start_month = getMonthStr(dtStart);
