dbf43f2ff693c03aec4b819ba84924e5f22f383e
Irvin <irvinfly@gmail.com>
2020-06-03 03:10:32 +0800

    event2page: fix url

--- events/event2page.js
+++ events/event2page.js
@@ -5,15 +5,16 @@ var template = fs.readFileSync('./event.handlebars', 'utf-8');
 template = handlebars.compile(template);
 
 let jsonData = require('./EVENTS.json');
-console.log('loaded', jsonData.length)
+console.log('loaded', jsonData.length);
 
 let year = process.argv[2] || '2020';
 let fn = `events_${year}.html`;
 
-var yearEvents =jsonData.filter((event) => {
-    return (event.start.toString()).startsWith(year)
+let yearEvents =jsonData.filter((event) => {
+    return (event.start.toString()).startsWith(year);
 });
-console.log('year:', year, yearEvents.length);
+let yearEventLen = yearEvents.length;
+console.log('year:', year, yearEventLen);
 
 
 // create file
@@ -22,7 +23,7 @@ let buffer;
 let top_template = fs.readFileSync('./page_top.handlebars', 'utf-8');
 top_template = handlebars.compile(top_template);
 
-buffer = top_template({ 'len': yearEvents.length, 'year': year });
+buffer = top_template({ 'len': yearEventLen, 'year': year });
 fs.writeFileSync(fn, buffer);    
 
 let getMonthStr = (dateObj) => ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'][dateObj.getMonth()];
@@ -30,11 +31,10 @@ let getMonthStr = (dateObj) => ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL',
 yearEvents.forEach(evt => { evt.start = new Date(evt.start); });
 yearEvents = yearEvents.sort((evt1, evt2) => evt1.start - evt2.start);
 
-yearEvents.forEach(evt => {
+yearEvents.forEach((evt, index) => {
 
     var dtStart = evt.start;
     var dtEnd = new Date(evt.end);
-
     evt.start_day = dtStart.getDate();
     evt.end_day = dtEnd.getDate();
     evt.start_month = getMonthStr(dtStart);
@@ -43,15 +43,16 @@ yearEvents.forEach(evt => {
     evt.multiday = (evt.start_day != evt.end_day);
     evt.multimonth = (evt.start_month != evt.end_month);
 
+    evt.slug = evt.remo_url.split('/e/')[1].slice(0, -1);
+
     // write to file
     var buffer = template(evt);
-    console.log(buffer);
+    console.log(`${index+1} / ${yearEventLen}`);
     var fn = `events_${year}.html`;
     fs.writeFileSync(fn, buffer, {flag: 'a'});    
 });
 
 buffer = fs.readFileSync('./page_bottom.handlebars', 'utf-8');
 fs.writeFileSync(fn, buffer, {flag: 'a'});    
-
 
 return;
